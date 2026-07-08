ed423b5c5dc2d63f7739e298251ad24045acf91c
AdityaDNikam <aditya.prakash.nikam.2004@gmail.com>
2026-07-09 01:32:31 +0530

    feat: implement user registration flow with multer file upload and cloudinary integration

--- Multer and Cloudinary steup/src/controller/user.controller.js
+++ Multer and Cloudinary steup/src/controller/user.controller.js
@@ -1,7 +1,60 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+import { FileUploadCloudinary } from "../utils/cloudinary.js";
+import ApiResponce from "../utils/ApiResponce.js";
 
 const reqisterUser = asyncHandler(async (req, res, next) => {
-    res.status(200).json({ message: "User reqisterd successfully" })
+    const { userName, email, password, fullname } = req.body
+    console.log(
+        "Email:", email
+    )
+    if ([userName, email, password, fullname].some(fields.trim() === "")) {
+        return ApiError(400, "All fields are Mandatory")
+    }
+    const ExistingUser = User.findOne({
+        $or: [{ userName }, { email }]
+    })
+
+    if (ExistingUser) {
+        throw new ApiError("400", "User with the username or email all ready exist!")
+    }
+    const avatarLocalPath = req.files?.avatar[0]?.[path]
+    const coverImgLocalPath = req.files?.coverImage[0]?.[path]
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar is mandatory")
+    }
+
+    const avatar = await FileUploadCloudinary(avatarLocalPath)
+    const coverImg = await FileUploadCloudinary(coverImgLocalPath)
+    if (!avatar) {
+        throw new ApiError(400, "Avatar is mandatory")
+    }
+
+    const user = await User.create({
+        userName: userName.toLowerCase(),
+        email,
+        password,
+        fullname,
+        avatar: avatar.url,
+        coverImage: coverImg?.url
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong")
+    }
+
+    return res.status(201).json(new ApiResponce(
+        201,
+        createdUser,
+        "User registered successfully"
+    ))
+
 })
 
 export { reqisterUser }
--- Multer and Cloudinary steup/src/routes/user.routes.js
+++ Multer and Cloudinary steup/src/routes/user.routes.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { reqisterUser } from "../controller/user.controller.js";
+import { upload } from "../middleware/multer.js"
 
 const router = Router()
 
-router.route("/register").post(reqisterUser)
+router.route("/register").post(
+    upload.fields([
+        { name: "avatar", maxCount: 1 },
+        { name: "coverImage", maxCount: 1 }
+    ]
+    ), reqisterUser)
 
 export default router
